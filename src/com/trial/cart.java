11f6069441663791f01656954743ce6375212daa
VCT-Rucha <ruchira.vsuryawanshi@gmail.com>
2022-10-15 05:47:18 +0530

    i inserted cart and logout class

---
+++ src/com/trial/cart.java
@@ -0,0 +1,7 @@
+package com.trial;
+
+public class cart {
+	public void adddProduct() {
+		System.out.println("I m adding product");
+	}
+}
---
+++ src/com/trial/logout.java
@@ -0,0 +1,7 @@
+package com.trial;
+
+public class logout {
+	 public void logout1() {
+		 System.out.println("i m logout");
+	 }
+}
